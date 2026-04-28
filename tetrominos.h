ca9df539872910770ebc65cec3ee82566d0f3a32
spiderLambo <spiderLambo.dev@outlook.com>
2026-04-28 13:09:12 +0200

    Update architecture (on a oublée de mettre une fonctionde base)

--- tetrominos.h
+++ tetrominos.h
@@ -3,4 +3,7 @@
 
 using tetromino = std::array <std::array <bool, 4>, 4>
 
+// Initialise un tetromino
+void genereTetro (tetromino & tetro, char type)
+
 void tournerTetro (tetromino & tetro, bool sens)
